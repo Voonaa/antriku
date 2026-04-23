2c8932ae1aac13f2e208b41051f2aea09af3fff0
Voonaa <agusss1618@gmail.com>
2026-04-24 06:26:09 +0700

    fix(websocket): fix bootstrap.js wsPort fallback from 8080 to 6001, fix forceTLS default to http

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -11,8 +11,8 @@ window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
     wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
+    wsPort: import.meta.env.VITE_REVERB_PORT ?? 6001,
+    wssPort: import.meta.env.VITE_REVERB_PORT ?? 6001,
+    forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'http') === 'https',
     enabledTransports: ['ws', 'wss'],
 });
